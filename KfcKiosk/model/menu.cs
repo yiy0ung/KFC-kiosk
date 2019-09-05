c030b2bb1cd5610073c53953d77e032f341232c5
wlsdud2194 <wlsdud2194@naver.com>
2019-09-05 14:02:46 +0900

    Update menu.cs

--- KfcKiosk/model/menu.cs
+++ KfcKiosk/model/menu.cs
@@ -11,5 +11,6 @@ namespace KfcKiosk.model
         public string Name { get; set; }
         public int Price { get; set; }
         public int Count { get; set; }
+        public string ImagePath { get; set; }
     }
 }
