ac90e28f5c0402ac6eab4a61a07848b6da487664
saehan <kmp1007s@naver.com>
2019-09-19 20:24:39 +0900

    Sort By Category
    카테고리 별 메뉴 정렬 기능 추가

--- KfcKiosk/PaymentWindow.xaml.cs
+++ KfcKiosk/PaymentWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kfc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,13 @@ namespace KfcKiosk
         public PaymentWindow()
         {
             InitializeComponent();
+            this.Loaded += PaymentWindow_Loaded;
+        }
+
+        private void PaymentWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            App.foodData.Load();
+            LoadMenu("All");
         }
 
         private void Prev_Window(object sender, RoutedEventArgs e)
@@ -32,14 +40,56 @@ namespace KfcKiosk
             main.Show();
         }
 
-        private void Burger_Click(object sender, RoutedEventArgs e)
+        private void LoadMenu(string selectedCategory)
         {
-            ShowBurger();
+            //Console.WriteLine(selectedCategory);
+            if (selectedCategory == null) return;
+
+            switch(selectedCategory)
+            {
+                case "Burger":
+                    SetMenu("Burger");
+                    break;
+
+                case "Chickin":
+                    SetMenu("Chickin");
+                    break;
+
+                case "Drink":
+                    SetMenu("Drink");
+                    break;
+
+                case "Snack":
+                    SetMenu("Snack");
+                    break;
+
+                default:
+                    SetMenu("All");
+                    break;
+            }
         }
 
-        private void ShowBurger()
+        private void SetMenu(string selectedCategory)
         {
-            
+            List<Food> foodList = new List<Food>();
+
+            foreach (Food food in App.foodData.lstMenu)
+            {
+                if (food.Category.ToString().Equals(selectedCategory) || selectedCategory.Equals("All"))
+                {
+                    foodList.Add(food);
+                } 
+            }
+
+            menuList.ItemsSource = foodList;
+        }
+
+        private void CategoryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedCategory = ((ListBoxItem)(categoryList.SelectedItem)).Content.ToString();
+
+            Console.WriteLine(selectedCategory);
+            LoadMenu(selectedCategory);
         }
     }
 }
