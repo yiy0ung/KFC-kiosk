4be3a3ec313fc39e6717c100795e0598c9559d48
Eun Chong Ju <vevsing@gmail.com>
2019-10-02 17:21:01 +0900

    Add files via upload
    일단 통계 화면 기틀만 올림

---
+++ KfcKiosk/Statistics.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KfcKiosk
+{
+	/// <summary>
+	/// Interaction logic for Statistics.xaml
+	/// </summary>
+	public partial class Statistics : Window
+	{
+		public Statistics()
+		{
+			InitializeComponent();
+		}
+		//public class SeatArgs : EventArgs
+		//{
+		//    public string TableId { get; set; }
+		//}
+	}
+}
